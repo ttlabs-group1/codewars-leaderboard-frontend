756b8fb26035f4c408c03448f45d67d68553494f
Selorm <selorm.lotsu@turntabl.io>
2023-01-10 18:42:53 +0000

    add login modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { LandingPage } from './pages/landing/landing.page';
 import { LeaderBoardPage } from './pages/leader-board/leader-board.page';
 import { UserDetailPage } from './pages/user-detail/user-detail.page';
 import { NotFoundPage } from './pages/not-found/not-found.page';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPage, LeaderBoardPage, UserDetailPage, NotFoundPage, NavbarComponent, FooterComponent],
+  declarations: [AppComponent, LandingPage, LeaderBoardPage, UserDetailPage, NotFoundPage, NavbarComponent, FooterComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
+    DynamicDialogModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent],
