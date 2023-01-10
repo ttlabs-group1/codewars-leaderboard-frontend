406cddcdb890637599605a46e74ff975f80a8409
Selorm <selorm.lotsu@turntabl.io>
2023-01-10 14:08:56 +0000

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { LandingPage } from './pages/landing/landing.page';
 import { LeaderBoardPage } from './pages/leader-board/leader-board.page';
 import { UserDetailPage } from './pages/user-detail/user-detail.page';
 import { NotFoundPage } from './pages/not-found/not-found.page';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPage, LeaderBoardPage, UserDetailPage, NotFoundPage],
+  declarations: [AppComponent, LandingPage, LeaderBoardPage, UserDetailPage, NotFoundPage, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
