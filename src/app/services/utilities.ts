b08df16f6f63f05687b2df24534850e2a3b393e2
Robert A <robert.ayetey@turntabl.io>
2023-01-11 14:34:48 +0000

    Update createObservable function to take generic param type

--- src/app/services/utilities.ts
+++ src/app/services/utilities.ts
@@ -1,11 +1,14 @@
-import { InjectionToken } from "@angular/core";
-import { Observable } from "rxjs";
+import { InjectionToken } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export const AUTH_SERVICE_TOKEN = new InjectionToken<string>('auth-service');
+export const USER_DETAIL_SERVICE_TOKEN = new InjectionToken<string>(
+  'user-detail-service'
+);
 
-export function createObservable(value: any): Observable<any> {
-    return new Observable<any>(subscriber => {
-        subscriber.next(value);
-        subscriber.complete();
-    });
+export function createObservable<T>(value: T): Observable<T> {
+  return new Observable<T>((subscriber) => {
+    subscriber.next(value);
+    subscriber.complete();
+  });
 }
