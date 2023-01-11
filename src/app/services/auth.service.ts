1ec723e7e5995281bba38765622d16cfc09aafc6
Selorm <selorm.lotsu@turntabl.io>
2023-01-11 14:55:03 +0000

    fix local auth service implementation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,5 +5,5 @@ import { User } from "../models/user.model";
 
 export interface AuthService {
     login(credentials: Auth): Observable<Response<User>>;
-    register(credentials: Auth): Observable<string>;
+    register(credentials: Auth): Observable<Response<string>>;
 }
